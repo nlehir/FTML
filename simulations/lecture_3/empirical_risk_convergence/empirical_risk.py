79a4845257f5bec476844f4e440f54ae81351e3f
nico <nicolaslehir@gmail.com>
2023-03-10 13:30:52 +0100

    update simulations/lecture_3/

--- simulations/lecture_3/empirical_risk_convergence/empirical_risk.py
+++ simulations/lecture_3/empirical_risk_convergence/empirical_risk.py
@@ -2,34 +2,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def f1(x):
-    if x == 1:
-        return 1
-    elif x == 0:
-        return 0
-    else:
-        raise ValueError("incorrect input")
+def f1(X: np.ndarray) -> np.ndarray:
+    y_pred = np.zeros(len(X))
+    y_pred[X == 1] = 1
+    y_pred[X == 0] = 0
+    return y_pred
 
 
-def f2(x):
-    if x == 1:
-        return 0
-    elif x == 0:
-        return 1
-    else:
-        raise ValueError("incorrect input")
+def f2(X: np.ndarray) -> np.ndarray:
+    y_pred = np.zeros(len(X))
+    y_pred[X == 1] = 0
+    y_pred[X == 0] = 1
+    return y_pred
 
 
-def f3(x):
-    if x == 1:
-        return 1
-    elif x == 0:
-        return 1
-    else:
-        raise ValueError("incorrect input")
+def f3(X: np.ndarray) -> np.ndarray:
+    y_pred = np.ones(len(X))
+    return y_pred
 
 
-def sample_dataset(n, p, q):
+def sample_dataset(n_samples, p, q):
     """
     Sample a dataset of n samples according to
     the joint law.
@@ -38,27 +30,36 @@ def sample_dataset(n, p, q):
     Y ~ B(p) if X=1
     Y ~ B(q) if X=0
     """
-    X = np.random.randint(0, 2, n)
-    Y = np.zeros(n)
-    for i in range(n):
-        if X[i] == 1:
-            y_i = np.random.binomial(1, p)
-        elif X[i] == 0:
-            y_i = np.random.binomial(1, q)
-        else:
-            raise ValueError("incorrect value of X")
-        Y[i] = y_i
-    return X, Y
+    rng = np.random.default_rng()
+
+    # generate input data
+    # they are uniformly distributed
+    # 0s and 1s
+    # If we do not cast to floats, we will have a mistake
+    # in the next step (all Bernoulli parameters would be
+    # set to 0)
+    X = rng.integers(0, 2, n_samples).astype(float)
+
+    # for each input, get the parameter of the
+    # corresponding Bernoulli law.
+    bernoulli = X.copy()
+    bernoulli[bernoulli == 1] = p
+    bernoulli[bernoulli == 0] = q
+
+    # generate output data
+    y = rng.binomial(1, bernoulli)
+
+    return X, y
 
 
-def compute_empirical_risk(f, X, Y):
+def compute_empirical_risk(f, X, y):
     """
     Compute empirical risk of predictor
     on the dataset
 
     Parameters:
         X: 1D array
-        Y: 1D array
+        y: 1D array
         f: predictor
 
     Returns:
@@ -66,49 +67,72 @@ def compute_empirical_risk(f, X, Y):
 
     We use the "0-1"-loss
     """
-    n_samples = len(X)
-    predictions = np.zeros(n_samples)
-    for i in range(n_samples):
-        predictions[i] = f(X[i])
-    return (predictions != Y).sum() / n_samples
+    n_samples = X.shape[0]
+    y_pred = f(X)
+    empirical_risk = len(np.where(y - y_pred)[0]) / n_samples
+    return empirical_risk
 
 
-p = 1 / 3
-q = 3 / 4
-max_n_samples = 1500
+def plot_convergence(X, y,generalization_error_f, f):
+    name = f.__name__
+    max_n_samples = len(X)
 
-generalization_error_f_1 = (1 - p) / 2 + q / 2
-generalization_error_f_2 = 1 - generalization_error_f_1
-generalization_error_f_3 = (1 - p) / 2 + (1 - q) / 2
+    # Monte carlo
+    empirical_risks = list()
+    for n in range(1, max_n_samples):
+        X_sample = X[:n]
+        y_sample = y[:n]
+        empirical_risks.append(compute_empirical_risk(f, X_sample, y_sample))
+
+    # plot results
+    plt.plot(
+        range(1, max_n_samples),
+        empirical_risks,
+        "o",
+        markersize=2,
+        alpha=0.3,
+        label=r"$R_n(f)$" + " empirical risk",
+    )
+    plt.plot(
+        range(1, max_n_samples),
+        (max_n_samples - 1) * [generalization_error_f],
+        color="hotpink",
+        label="real risk / generalization error",
+    )
+
+    # finish plot
+    plt.xlabel("number of samples")
+    plt.legend(loc="best")
+    plt.title(
+        f"{name}\n"
+        "Convergence of the empirical risk to the real risk"
+        + "\n"
+        + f"R({name})"
+        + f"={generalization_error_f:.2f}"
+    )
+    figname = f"empirical_risk_and_generalization_error_{name}.pdf"
+    plt.tight_layout()
+    plt.savefig(figname)
+    plt.close()
 
 
-# test f3
-empirical_risks = list()
-for n in range(1, max_n_samples):
-    X, Y = sample_dataset(n, p, q)
-    empirical_risks.append(compute_empirical_risk(f3, X, Y))
-plt.plot(
-    range(1, max_n_samples),
-    empirical_risks,
-    "o",
-    markersize=2,
-    alpha=0.3,
-    label=r"$R_n(f)$" + " empirical risk",
-)
-plt.plot(
-    range(1, max_n_samples),
-    (max_n_samples - 1) * [generalization_error_f_3],
-    color="hotpink",
-    label="real risk / generalization error",
-)
+def main():
+    # constants
+    p = 1 / 3
+    q = 3 / 4
 
-plt.xlabel("number of samples")
-plt.legend(loc="best")
-plt.title(
-    # r"$f_3$"
-    "Convergence of the empirical risk to the real risk"
-    # + "\n"
-    # + f"$R(f_3)$"
-    # + f"={generalization_error_f_3:.2f}"
-)
-plt.savefig("empirical_risk_and_generalization_error.pdf")
+    # sample the dataset
+    max_n_samples = int(3e3)
+    X, y = sample_dataset(max_n_samples, p, q)
+
+    generalization_error_f_1 = (1 - p) / 2 + q / 2
+    generalization_error_f_2 = 1 - generalization_error_f_1
+    generalization_error_f_3 = (1 - p) / 2 + (1 - q) / 2
+
+    plot_convergence(X, y, generalization_error_f_1, f1)
+    plot_convergence(X, y, generalization_error_f_2, f2)
+    plot_convergence(X, y, generalization_error_f_3, f3)
+
+
+if __name__ == "__main__":
+    main()
