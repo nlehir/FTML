b7b4bbe377cd2ac01f6b73b75c8601025f88c56b
Nicolas Le Hir <nicolaslehir@gmail.com>
2024-03-14 20:06:46 +0100

    clean practical_sessions/

--- practical_sessions/tp5_kmeans_sgd_line_search/line_search/optimal_gamma.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-def compute_gamma_star(H, gradient):
-    """
-    Line search gamma
-    Fix this function
-    """
-    return 1
--- practical_sessions/tp5_kmeans_sgd_line_search/lr/learning_rate_schedule.py
+++
@@ -1,8 +0,0 @@
-"""
-    Set the schedule of the learning rate during GD or SGD
-"""
-import math
-
-
-def learning_rate_schedule(gamma_0, iteration, schedule) -> float:
-    return gamma_0
