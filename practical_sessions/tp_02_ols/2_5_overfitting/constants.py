f61e98447274ccba1c0db12b904b9df236dbe4ad
nlehir <nicolaslehir@gmail.com>
2026-04-22 23:33:09 +0200

    add tp 02

---
+++ practical_sessions/tp_02_ols/2_5_overfitting/constants.py
@@ -0,0 +1,3 @@
+# amount of noise (linear model, fixed design)
+SIGMA = 8
+BAYES_RISK = SIGMA**2
---
+++ practical_sessions/tp_02_ols/2_5_overfitting/main_ols.py
@@ -0,0 +1,56 @@
+"""
+Study the statistical properties of the OLS estimator:
+    - excess risk
+    - dependence of the risk on the dimensions n and d
+    - (optionally) stability of the OLS estimator
+"""
+
+# from utils_algo_solution import ols_test_error
+from utils_algo import ols_test_error
+
+# from utils_algo import ols_test_error
+from utils_plots import plot_test_errors_ols
+
+# FIX
+
+# FIX
+
+# TODO
+
+# FIX
+
+# FIX
+
+# FIX
+
+# FIX
+
+
+def main() -> None:
+    # dimensions of the problem
+    n_train_list = list(range(30, 200, 2))
+    d_list = [2, 5, 10, 20, 30]
+    n_repetitions_list = [10, 100, 1000, 10000]
+
+    # Assess the influence of different values of n and d
+    # store the test errors in a dict, each key being a (n, d) pair
+    test_errors = dict()
+    for n_repetitions in n_repetitions_list:
+        print(f"{n_repetitions=}")
+        for n_train in n_train_list:
+            for d in d_list:
+                test_errors[(n_train, d)] = ols_test_error(
+                    n_train=n_train,
+                    d=d,
+                    n_repetitions=n_repetitions,
+                )
+        plot_test_errors_ols(
+            test_errors=test_errors,
+            n_list=n_train_list,
+            d_list=d_list,
+            n_repetitions=n_repetitions,
+        )
+
+
+if __name__ == "__main__":
+    main()
